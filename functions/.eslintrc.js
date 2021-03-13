a221eb4504cf6c39d0bfa17f7cbeba8b2e36a7a3
tracer482 <Mustang69>
2021-03-12 19:36:02 -0700

    fixing eslint customization adding helloworld function ignoreing .firebase

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -4,11 +4,10 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ['eslint:recommended', 'google'],
   rules: {
-    quotes: ["error", "double"],
+    quotes: ['error', 'single'],
+    'object-curly-spacing': ['error', 'allways'],
+    'quote-props': ['error', 'never'],
   },
 };
