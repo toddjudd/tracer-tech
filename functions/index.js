572c0765bb5b9010c4f6a14d102b11278b213773
toddjudd <toddjudd@gmail.com>
2021-03-24 21:36:07 -0600

    json funciotns fake oauth func hello auth that need auth

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,7 @@ admin.initializeApp();
 const express = require('express');
 const cookieParser = require('cookie-parser')();
 const cors = require('cors')({ origin: true });
-const app = express();
+const api = express();
 
 // Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
 // The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
@@ -76,15 +76,26 @@ const validateFirebaseIdToken = async (req, res, next) => {
   }
 };
 
-app.use(cors);
-app.use(cookieParser);
-app.use(validateFirebaseIdToken);
-app.get('/hello', (req, res) => {
-  // @ts-ignore
+api.use(cors);
+api.use(cookieParser);
+api.post('/login/oauth/access_token', (req, res) => {
+  userAccessToken =
+    'eyJhbGciOiJSUzI1NiIsImtpZCI6ImY4NDY2MjEyMTQxMjQ4NzUxOWJiZjhlYWQ4ZGZiYjM3ODYwMjk5ZDciLCJ0eXAiOiJKV1QifQ';
+  res.send(`access_token=${userAccessToken}&token_type=bearer
+  `);
+});
+api.post('/json', (req, res) => {
+  res.status(200).send({ header, body, cookies });
+});
+api.get('/json', (req, res) => {
+  res.status(200).send({ header, params, cookies });
+});
+api.use(validateFirebaseIdToken);
+api.get('/hello', (req, res) => {
   res.send(`Hello ${req.user.name}`);
 });
 
 // This HTTPS endpoint can only be accessed by your Firebase Users.
 // Requests need to be authorized by providing an `Authorization` HTTP header
 // with value `Bearer <Firebase ID Token>`.
-exports.app = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(api);
