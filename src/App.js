0777297997e9c99745c34c149a0ee1dd0bd89e76
toddjudd <toddjudd@gmail.com>
2021-03-28 23:16:52 -0600

    working on a default oauth flow to allow apps to connect via oauth.

--- src/App.js
+++ src/App.js
@@ -1,83 +1,14 @@
-import logo from './Tracer.svg';
+import LoggedIn from './LoggedIn';
+import LoggedOut from './LoggedOut';
 import './App.css';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { Button } from 'react-bootstrap';
+import auth from './firebase';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
-// import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
-
-firebase.initializeApp({
-  apiKey: 'AIzaSyBRcMvWr5ebXGpYzrvKeqFg2Ua1EKKgiP0',
-  authDomain: 'tracer-tech.firebaseapp.com',
-  databaseURL: 'https://tracer-tech-default-rtdb.firebaseio.com',
-  projectId: 'tracer-tech',
-  storageBucket: 'tracer-tech.appspot.com',
-  messagingSenderId: '554765066642',
-  appId: '1:554765066642:web:66f145f9193b5f4806214b',
-});
-
-const auth = firebase.auth();
-// const firestore = firebase.firestore();
 
 const App = () => {
   const [user] = useAuthState(auth);
-
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <SignOut />
-      </header>
-      <section className='App-section'>
-        {user ? <Welcome /> : <SignIn />}
-      </section>
-    </div>
-  );
-};
-
-const SignIn = () => {
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  };
-  return (
-    <>
-      <img src={logo} className='App-logo-main' alt='logo' />
-      <Button className='sign-in' onClick={signInWithGoogle}>
-        Sign In With Google <FontAwesomeIcon icon={faGoogle} />
-      </Button>
-    </>
-  );
-};
-
-const SignOut = () => {
-  return (
-    auth.currentUser && (
-      <>
-        <img src={logo} className='App-logo-icon' alt='logo' />
-        <Button
-          className='sign-out'
-          onClick={() => {
-            auth.signOut();
-          }}>
-          Sign Out
-        </Button>
-      </>
-    )
-  );
-};
-
-const Welcome = () => {
-  console.log(auth.currentUser.getIdToken(true));
-  return (
-    <>
-      <h1>Welcome {auth.currentUser.displayName}</h1>
-    </>
-  );
+  return <div className='App'>{user ? <LoggedIn /> : <LoggedOut />}</div>;
 };
 
 export default App;
